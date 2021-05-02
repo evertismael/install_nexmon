cd0d67165079b8ec1d9204a02dd32a72ad16eff9
evertismael <evertismael@gmail.com>
2021-05-02 13:55:04 +0100

    echos

--- 0010_links.sh
+++ 0010_links.sh
@@ -17,6 +17,7 @@ setStatus "Downloading Nexmon"
 cd nexmon
 # git checkout f9db9abcac8f40a7f8a8408429e34e1c51f33c97
 NEXDIR=$(pwd)
+echo 'pwd'
 
 setStatus "Building libISL"
 cd $NEXDIR/buildtools/isl-0.10
@@ -25,6 +26,7 @@ autoreconf -f -i
 make
 make install
 ln -s /usr/local/lib/libisl.so /usr/lib/arm-linux-gnueabihf/libisl.so.10
+echo 'libISL'
 
 setStatus "Building libMPFR"
 cd $NEXDIR/buildtools/mpfr-3.1.4
@@ -33,5 +35,7 @@ autoreconf -f -i
 make
 make install
 ln -s /usr/local/lib/libmpfr.so /usr/lib/arm-linux-gnueabihf/libmpfr.so.4
+echo 'libMPFR'
 
 setStatus "Completed 0010"
+echo 'completed 0010'
