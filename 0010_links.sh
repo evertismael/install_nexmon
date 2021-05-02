d3dc687b32d3efbb8ef57209dcabde57ae641e35
root <evertismael@gmail.com>
2021-05-02 13:43:38 +0100

    s

---
+++ 0010_links.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# @author: Aravind Voggu @zeroby0
+
+function setStatus () {
+  if { [ "$TERM" = "screen" ] && [ -n "$TMUX" ]; } then
+    tmux set status-right "Status: $1"
+  else
+    echo "Status: $1"
+  fi
+}
+
+setStatus "Removing wpasupplicant"
+apt remove wpasupplicant -y
+echo "denyinterfaces wlan0" >> /etc/dhcpcd.conf
+
+setStatus "Downloading Nexmon"
+cd nexmon
+# git checkout f9db9abcac8f40a7f8a8408429e34e1c51f33c97
+NEXDIR=$(pwd)
+
+setStatus "Building libISL"
+cd $NEXDIR/buildtools/isl-0.10
+autoreconf -f -i
+./configure
+make
+make install
+ln -s /usr/local/lib/libisl.so /usr/lib/arm-linux-gnueabihf/libisl.so.10
+
+setStatus "Building libMPFR"
+cd $NEXDIR/buildtools/mpfr-3.1.4
+autoreconf -f -i
+./configure
+make
+make install
+ln -s /usr/local/lib/libmpfr.so /usr/lib/arm-linux-gnueabihf/libmpfr.so.4
+
+setStatus "Completed 0010"
